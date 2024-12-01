cb75fd7eaa1002a6ed3e1e16d6c49e253a8434d7
Tyrion <wltjd9407@naver.com>
2024-12-01 22:52:03 +0900

    aws health check

--- src/main/java/com/Mutation/Tr/main/controller/MainController.java
+++ src/main/java/com/Mutation/Tr/main/controller/MainController.java
@@ -3,6 +3,7 @@ package com.Mutation.Tr.main.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,5 +18,8 @@ public class MainController {
         System.err.println("main");
         return mainHtmlContentPath+"index";
     }
-
+    @GetMapping("/health")  // 또는 로드밸런서가 체크하는 경로
+    public String healthCheck() {
+        return "OK";  // 간단한 응답
+    }
 }
